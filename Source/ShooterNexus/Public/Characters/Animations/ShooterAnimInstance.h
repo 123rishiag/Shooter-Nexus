513d8bd684dd7e705f24c9a0564e430ba097029b
Rishabh Garg <narishabh.garg@gmail.com>
2025-07-26 01:21:17 +0530

    Implemented Character Movement & Jump Animations

---
+++ Source/ShooterNexus/Public/Characters/Animations/ShooterAnimInstance.h
@@ -0,0 +1,33 @@
+// Shooter Nexus
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+class AShooterCharacter;
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTERNEXUS_API UShooterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+	UPROPERTY(BlueprintReadOnly, Category = "Character", meta = (AllowPrivateAccess = "true"))
+	AShooterCharacter* ShooterCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed = 0.f;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir = false;
+	UPROPERTY(BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsAccelerating = false;
+};
